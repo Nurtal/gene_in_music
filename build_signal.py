4aac93ba6fdff28f2d2f38b147059e898eb68973
Nurtal <nathan.foulquier.pro@gmail.com>
2025-04-05 14:53:18 +0200

    turn signal into audio file

--- build_signal.py
+++ build_signal.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import random
 import os
-
+from scipy.io.wavfile import write
+from scipy.interpolate import interp1d
 
 def build_random_signal(data_file:str):
     """Build signals from data file, assign random order and random interval to genes
@@ -58,11 +59,46 @@ def build_random_signal(data_file:str):
             output_file.write(f"{x},{y[cmpt]}\n")
             cmpt+=1
         output_file.close()
+
+
+
+def turn_signal_into_audio(signal_file:str, target_duration:float) -> None:
+    """Turn a signal extracted from data file to an audio signal and save it in
+    a wave file
     
+    Args:
+        - signal_file (str) : path to a signal file
+        - target_duration (float) : duration of the audio fole
+    
+    """
+
+    # load signal
+    df = pd.read_csv(signal_file)
+    x = np.array(list(df['x']))
+    y = np.array(list(df['y']))
+
+    # Redimensionner x pour fit la target_duration
+    x = (x - x[0]) / (x[-1] - x[0]) * target_duration
+
+    # audio stuff
+    sample_rate = 44100  # 44.1 kHz
+    duration = x[-1]
+    t = np.linspace(0, duration, int(sample_rate * duration))
+
+    # Interpolation pour obtenir des y réguliers
+    interpolateur = interp1d(x, y, kind='linear')
+    y_interp = interpolateur(t)
+
+    # Normaliser pour correspondre à une plage 16-bit
+    y_norm = np.int16(y_interp / np.max(np.abs(y_interp)) * 32767)
+
+    # Sauvegarder dans un fichier WAV
+    write(signal_file.replace(".csv", ".wav"), sample_rate, y_norm)
 
 
 if __name__ == "__main__":
 
-    build_random_signal("data/toy_data.csv")
-
+    # build_random_signal("data/toy_data.csv")
+    
+    turn_signal_into_audio("signals/42_signal.csv", 4.0)
     
