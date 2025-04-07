5cd9000c7f03d9c6c8e727c6f9b3e6ce82e0ae1f
Nurtal <nathan.foulquier.pro@gmail.com>
2025-04-07 11:45:57 +0200

    compute prox gene matrix from correlation #21

---
+++ extract_gene_order.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+
+def get_proximity_from_data(data_file_list:list, matrix_save_file:str) -> None:
+    """Compute proximity beween genes as the absolute correlation of genes expression within the merged datasets
+
+    Args:
+        - data_file_list (list) : list of path of datasets to use
+        - matrix_save_file (str) : path to save the matrix as a csv file
+    
+    """
+
+    # load and merge data
+    dfs = [pd.read_csv(f, index_col=0) for f in data_file_list]
+    df = pd.concat(dfs, axis=0)
+
+    # compute absolute correlation matrix
+    corr_matrix = df.corr(method='pearson')  # corrélation entre colonnes
+    abs_corr = corr_matrix.abs()
+    abs_corr = abs_corr.fillna(0)
+
+    # save matrix
+    abs_corr.to_csv(matrix_save_file)
+
+
+def build_order_from_proximity():
+    """ """
+
+
+if __name__ == "__main__":
+
+    
+    get_proximity_from_data(['data/gene_reads_artery_aorta.csv', 'data/gene_reads_artery_coronary.csv'], "data/prox_matrix.csv")
+    
