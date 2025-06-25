ccf35562f734ac4105613e1f273a0080f5006ad4
Nurtal <nathan.foulquier.pro@gmail.com>
2025-06-25 13:26:21 +0200

    add pca

--- preprocess_benchmark.py
+++ preprocess_benchmark.py
@@ -1,8 +1,34 @@
 import os
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 
-def get_pca(df):
-    """ """ 
+def get_pca(df:pd.Dataframe) -> pd.DataFrame:
+    """Compute PCA from a pandas dataframe, return coordinates in the PCA space""" 
+
+    # Extract
+    ids = df['ID']
+    labels = df['LABEL']
+    X = df.drop(columns=['ID', 'LABEL'])
+
+    # Normalize
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    # Compute PCA
+    pca = PCA(n_components=2)
+    X_pca = pca.fit_transform(X_scaled)
+
+    # craft & return df
+    df = pd.DataFrame({
+        'ID': ids,
+        'LABEL': labels,
+        'PC1': X_pca[:, 0],
+        'PC2': X_pca[:, 1]
+    })
+    return df
+
 
 def run(data_file, output_dir):
     """ """
@@ -16,10 +42,11 @@ def run(data_file, output_dir):
     df = pd.read_csv(data_file)
 
     # save raw data
-    df.to_csv(f"{output_dir}/data/raw.csv")
+    df.to_csv(f"{output_dir}/data/raw.csv", index=False)
 
-    # save PCA
-    get_pca(df)
+    # generate & save PCA
+    df_pca = get_pca(df)
+    df_pca.to_csv(f"{output_dir}/data/pca.csv", index=False)
 
     # save umap daya
 
@@ -27,7 +54,6 @@ def run(data_file, output_dir):
 
     
 
-    print("Tardis")
 
 
 
