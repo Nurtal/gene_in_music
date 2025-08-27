2eecaf289e891da7e8b382cf8613ff5d8151527d
Nurtal <nathan.foulquier.pro@gmail.com>
2025-08-27 16:06:02 +0200

    update kmeans

--- get_cluster.py
+++ get_cluster.py
@@ -1,8 +1,10 @@
 import glob
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score, adjusted_rand_score, normalized_mutual_info_score
 
 
 
+# import local module
 import extract_features
 
 
@@ -30,7 +32,7 @@ def run_kmeans(file_list:list, J:int, Q:int, result_file:str, audio_duration:flo
     kmeans = KMeans(n_clusters=k, random_state=42, n_init="auto")
     y_pred = kmeans.fit_predict(X)
 
-    # save results
+    # save prediction
     i = 0
     prediction = open(result_file, "w")
     prediction.write("FILE,LABEL\n")
@@ -38,7 +40,22 @@ def run_kmeans(file_list:list, J:int, Q:int, result_file:str, audio_duration:flo
         prediction.write(f"{f},{y_pred[i]}\n")
         i+=1
     prediction.close()
-        
+
+    # compute & save silhouette score
+    sil_score = silhouette_score(X, y_pred)
+    metric_file = open(f"{result_file.replace('.csv', '.log')}", "w")
+    metric_file.write(f"Silouhette_score : {sil_score}\n")
+    metric_file.close()
+    
+    
+
+
+def evaluate_clustering(prediction_file, manifest_file):
+    """ """
+
+
+    sil_score = silhouette_score(X, y_pred)
+
 
 
 if __name__ == "__main__":
